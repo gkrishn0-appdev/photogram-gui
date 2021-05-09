5a5ddd717291925f89639736993079d080640c28
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-05-09 18:23:06 +0000

    88.57

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,5 +25,20 @@ class UsersController < ApplicationController
 
     #render({:template => "photo_templates/create.html.erb"})
   end
+
+  def update
+    the_id = params.fetch("modify_id")
+    matching_users=User.where({:id => the_id})
+    the_user = matching_users.at(0)
+    input_image = params.fetch("query_image")
+    #input_caption = params.fetch("query_caption")
+    #input_owner_id = params.fetch("query_owner_id")
+
+    the_user.username=input_image
+    #the_photo.caption = input_caption
+    the_user.save
+    redirect_to("/users/" + the_user.username)
+    #render({:template => "photo_templates/update.html.erb"})
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
    get("/update_photo/:modify_id", {:controller => "photos", :action => "update"})
 
    get("/insert_user", {:controller => "users", :action => "create"})
+   get("/update_user/:modify_id", {:controller => "users", :action => "update"})
 end
